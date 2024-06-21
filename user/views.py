2fe561edb48be9244319ea094bedb282e61c9f94
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-22 00:03:21 +0300

    user interface rendering feature implemented

--- user/views.py
+++ user/views.py
@@ -1,1 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.settings import api_settings
+
+from user.serializers import UserSerializer
+
+
+class LoginUserView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
