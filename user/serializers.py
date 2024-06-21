c85bb3880f21bfb6e041935280439754f8b390bc
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-22 00:20:55 +0300

    serializer bugfix

--- user/serializers.py
+++ user/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "first_name", "last_name", "password", "tokens")
+        fields = ("id", "email", "first_name", "last_name", "password")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
