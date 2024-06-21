73b2093e0f80331b8be346906efe543ff49e01dd
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-22 00:28:22 +0300

    email utils implemented

--- user/serializers.py
+++ user/serializers.py
@@ -5,8 +5,8 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "email", "first_name", "last_name", "password")
-        read_only_fields = ("id", "is_staff")
+        fields = ("id", "email", "first_name", "last_name", "password", "is_verified")
+        read_only_fields = ("id", "is_staff", "is_verified")
         extra_kwargs = {"password": {"write_only": True, "min_length": 5}}
 
     def create(self, validated_data):
--- user/urls.py
+++ user/urls.py
@@ -1,15 +1,19 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 from user.views import ManageUserView
 
-app_name = 'user'
+app_name = "user"
 
 
 urlpatterns = [
     path("me/", ManageUserView.as_view(), name="my_info"),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
