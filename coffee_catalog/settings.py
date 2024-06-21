3223b7e782a0b06f7358d570fad40c8f67c2a0d1
twonumberfortyfives <sma4no.7@gmail.com>
2024-06-21 23:59:45 +0300

    implemented user models

--- coffee_catalog/settings.py
+++ coffee_catalog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework_simplejwt",
     "rest_framework",
     "user",
 ]
@@ -123,3 +124,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    )
+}
+
